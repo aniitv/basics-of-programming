ff610afa3b6b9fcac3d4a757ec30afbfb94ae263
Anastasiia Zotova <anastasiiaazotova@gmail.com>
2025-11-26 23:48:18 +0200

    add

---
+++ lab6/1-pipe.js
@@ -0,0 +1,19 @@
+"use strict";
+const inc = (x) => ++x;
+const twice = (x) => x * 2;
+const cube = (x) => x ** 3;
+
+const pipe = (...fns) => {
+  for (const f of fns) {
+    if (typeof f !== "function") {
+      throw new Error("All compose arguments should be functions");
+    }
+  }
+  return (x) => fns.reduce((v, f) => f(v), x);
+};
+
+const f1 = pipe(inc, twice, cube);
+console.log(f1(5));
+
+const f2 = pipe(inc, inc);
+console.log(f2(7));
