d088fef67c22498dbc348f8f94b3a91980cdecfa
Anastasiia Zotova <anastasiiaazotova@gmail.com>
2025-12-06 13:28:13 +0200

    fix 3-wrapper.js

--- lab8/3-wrapper.js
+++ lab8/3-wrapper.js
@@ -1,31 +1,25 @@
 "use strict";
 
-function contract(fn, ...types) {
-  for (let type of types) {
-    return function (...args) {
-      for (let i = 0; i < types.length - 1; i++) {
-        if (typeof args[i] !== types[i].name.toLowerCase()) {
-          throw new TypeError();
-        }
+const contract =
+  (fn, ...types) =>
+  (...args) => {
+    for (let i = 0; i < args.length; i++) {
+      const arg = args[i];
+      const def = types[i];
+      const name = def.name.toLowerCase();
+      if (typeof arg !== name) {
+        throw new TypeError(`Argument type ${name} expected`);
       }
-
-      const result = fn(...args);
-
-      if (typeof result !== types[types.length - 1].name.toLowerCase()) {
-        throw new TypeError();
-      }
-
-      return result;
-    };
-  }
-}
-
+    }
+    const res = fn(...args);
+    const def = types[types.length - 1];
+    const name = def.name.toLowerCase();
+    if (typeof res !== name) {
+      throw new TypeError(`Result type ${name} expected`);
+    }
+    return res;
+  };
 const add = (a, b) => a + b;
 const addNumbers = contract(add, Number, Number, Number);
 const res1 = addNumbers(2, 3);
-console.dir(res1);
-
-const concat = (s1, s2) => s1 + s2;
-const concatStrings = contract(concat, String, String, String);
-const res2 = concatStrings("Hello ", "world!");
-console.dir(res2);
+console.log(res1);
